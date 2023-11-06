f477ee754138d6c86059a9745576561c82bba9e5
Mikastiv <mikastiv@outlook.com>
2023-11-06 00:15:38 -0500

    simpler matrix mul code

--- src/math.zig
+++ src/math.zig
@@ -188,132 +188,26 @@ pub const mat = struct {
     }
 
     pub inline fn mul(a: anytype, b: @TypeOf(a)) @TypeOf(a) {
-        return switch (@TypeOf(a)) {
-            Mat2 => blk: {
-                const a00 = a[0][0];
-                const a01 = a[0][1];
-                const a10 = a[1][0];
-                const a11 = a[1][1];
-
-                const b00 = b[0][0];
-                const b01 = b[0][1];
-                const b10 = b[1][0];
-                const b11 = b[1][1];
-
-                break :blk .{
-                    .{
-                        a00 * b00 + a10 * b01,
-                        a01 * b00 + a11 * b01,
-                    },
-                    .{
-                        a00 * b10 + a10 * b11,
-                        a01 * b10 + a11 * b11,
-                    },
-                };
-            },
-            Mat3 => blk: {
-                const a00 = a[0][0];
-                const a01 = a[0][1];
-                const a02 = a[0][2];
-                const a10 = a[1][0];
-                const a11 = a[1][1];
-                const a12 = a[1][2];
-                const a20 = a[2][0];
-                const a21 = a[2][1];
-                const a22 = a[2][2];
-
-                const b00 = b[0][0];
-                const b01 = b[0][1];
-                const b02 = b[0][2];
-                const b10 = b[1][0];
-                const b11 = b[1][1];
-                const b12 = b[1][2];
-                const b20 = b[2][0];
-                const b21 = b[2][1];
-                const b22 = b[2][2];
-
-                break :blk .{
-                    .{
-                        a00 * b00 + a10 * b01 + a20 * b02,
-                        a01 * b00 + a11 * b01 + a21 * b02,
-                        a02 * b00 + a12 * b01 + a22 * b02,
-                    },
-                    .{
-                        a00 * b10 + a10 * b11 + a20 * b12,
-                        a01 * b10 + a11 * b11 + a21 * b12,
-                        a02 * b10 + a12 * b11 + a22 * b12,
-                    },
-                    .{
-                        a00 * b20 + a10 * b21 + a20 * b22,
-                        a01 * b20 + a11 * b21 + a21 * b22,
-                        a02 * b20 + a12 * b21 + a22 * b22,
-                    },
-                };
-            },
-            Mat4 => blk: {
-                const a00 = a[0][0];
-                const a01 = a[0][1];
-                const a02 = a[0][2];
-                const a03 = a[0][3];
-                const a10 = a[1][0];
-                const a11 = a[1][1];
-                const a12 = a[1][2];
-                const a13 = a[1][3];
-                const a20 = a[2][0];
-                const a21 = a[2][1];
-                const a22 = a[2][2];
-                const a23 = a[2][3];
-                const a30 = a[3][0];
-                const a31 = a[3][1];
-                const a32 = a[3][2];
-                const a33 = a[3][3];
-
-                const b00 = b[0][0];
-                const b01 = b[0][1];
-                const b02 = b[0][2];
-                const b03 = b[0][3];
-                const b10 = b[1][0];
-                const b11 = b[1][1];
-                const b12 = b[1][2];
-                const b13 = b[1][3];
-                const b20 = b[2][0];
-                const b21 = b[2][1];
-                const b22 = b[2][2];
-                const b23 = b[2][3];
-                const b30 = b[3][0];
-                const b31 = b[3][1];
-                const b32 = b[3][2];
-                const b33 = b[3][3];
-
-                break :blk .{
-                    .{
-                        a00 * b00 + a10 * b01 + a20 * b02 + a30 * b03,
-                        a01 * b00 + a11 * b01 + a21 * b02 + a31 * b03,
-                        a02 * b00 + a12 * b01 + a22 * b02 + a32 * b03,
-                        a03 * b00 + a13 * b01 + a23 * b02 + a33 * b03,
-                    },
-                    .{
-                        a00 * b10 + a10 * b11 + a20 * b12 + a30 * b13,
-                        a01 * b10 + a11 * b11 + a21 * b12 + a31 * b13,
-                        a02 * b10 + a12 * b11 + a22 * b12 + a32 * b13,
-                        a03 * b10 + a13 * b11 + a23 * b12 + a33 * b13,
-                    },
-                    .{
-                        a00 * b20 + a10 * b21 + a20 * b22 + a30 * b23,
-                        a01 * b20 + a11 * b21 + a21 * b22 + a31 * b23,
-                        a02 * b20 + a12 * b21 + a22 * b22 + a32 * b23,
-                        a03 * b20 + a13 * b21 + a23 * b22 + a33 * b23,
-                    },
-                    .{
-                        a00 * b30 + a10 * b31 + a20 * b32 + a30 * b33,
-                        a01 * b30 + a11 * b31 + a21 * b32 + a31 * b33,
-                        a02 * b30 + a12 * b31 + a22 * b32 + a32 * b33,
-                        a03 * b30 + a13 * b31 + a23 * b32 + a33 * b33,
-                    },
-                };
-            },
+        switch (@TypeOf(a)) {
+            Mat2, Mat3, Mat4 => {},
             else => unsupportedType(@TypeOf(a)),
-        };
+        }
+
+        const row_len = veclen(@TypeOf(a[0]));
+        const col_len = veclen(@TypeOf(a[0]));
+
+        var out: @TypeOf(a) = undefined;
+        inline for (0..row_len) |row| {
+            inline for (0..col_len) |col| {
+                const v = b[row];
+                out[row][col] = 0;
+                inline for (0..col_len) |i| {
+                    out[row][col] += a[i][col] * v[i];
+                }
+            }
+        }
+
+        return out;
     }
 
     pub inline fn scaling(s: Vec3) Mat4 {
