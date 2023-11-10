c1936aad5fb77f0278dc43d27df8ff35e48c46ed
Mikastiv <mikastiv@outlook.com>
2023-11-10 13:55:16 -0500

    fix bmp code

--- src/bmp.zig
+++ src/bmp.zig
@@ -34,46 +34,22 @@ const FileHeader = extern struct {
 };
 
 const BmpHeader = extern struct {
-    header_size: u32,
-    width: i32,
-    height: i32,
-    n_color_planes: u16,
-    bpp: u16,
-    compression_type: CompressionType,
-    image_size: u32,
-    x_pixels_per_meter: u32,
-    y_pixels_per_meter: u32,
-    n_colors: u32,
-    n_important_colors: u32,
-    red_mask: u32,
-    green_mask: u32,
-    blue_mask: u32,
-    alpha_mask: u32,
-    color_space: ColorSpace,
-};
-
-const BufferedReader = std.io.BufferedReader(8 * 1024, std.io.StreamSource.Reader);
-
-const BmpReader = struct {
-    const Self = @This();
-
-    buffered_reader: BufferedReader,
-
-    fn init(stream: *std.io.StreamSource) Self {
-        return .{ .buffered_reader = .{ .unbuffered_reader = stream.reader() } };
-    }
-
-    fn readFn(self: *const anyopaque, buffer: []u8) anyerror!usize {
-        const ptr: *Self = @constCast(@alignCast(@ptrCast(self)));
-        return ptr.buffered_reader.read(buffer);
-    }
-
-    fn reader(self: *Self) std.io.AnyReader {
-        return .{
-            .context = @ptrCast(self),
-            .readFn = readFn,
-        };
-    }
+    header_size: u32 align(1),
+    width: i32 align(1),
+    height: i32 align(1),
+    n_color_planes: u16 align(1),
+    bpp: u16 align(1),
+    compression_type: CompressionType align(1),
+    image_size: u32 align(1),
+    x_pixels_per_meter: u32 align(1),
+    y_pixels_per_meter: u32 align(1),
+    n_colors: u32 align(1),
+    n_important_colors: u32 align(1),
+    red_mask: u32 align(1),
+    green_mask: u32 align(1),
+    blue_mask: u32 align(1),
+    alpha_mask: u32 align(1),
+    color_space: ColorSpace align(1),
 };
 
 pub fn load(allocator: mem.Allocator, filename: []const u8, flip_vertically: bool) !Image {
@@ -81,12 +57,11 @@ pub fn load(allocator: mem.Allocator, filename: []const u8, flip_vertically: boo
 
     const image = try std.fs.cwd().openFile(filename, .{});
     defer image.close();
-    var stream = std.io.StreamSource{ .file = image };
-    var bmp_reader = BmpReader.init(&stream);
-    var reader = bmp_reader.reader();
 
-    const file_header: FileHeader = try reader.readStruct(FileHeader);
-    const bmp_header: BmpHeader = try reader.readStruct(BmpHeader);
+    const image_data = try image.readToEndAlloc(allocator, std.math.maxInt(usize));
+
+    const file_header: *const FileHeader = @alignCast(@ptrCast(image_data.ptr));
+    const bmp_header: *const BmpHeader = @alignCast(@ptrCast(image_data.ptr + @sizeOf(FileHeader)));
 
     if (bmp_header.header_size < @sizeOf(BmpHeader))
         return error.UnsupportedHeaderVersion;
@@ -104,36 +79,41 @@ pub fn load(allocator: mem.Allocator, filename: []const u8, flip_vertically: boo
         return error.UnsupportedColorSpace;
 
     var flipped = bmp_header.height < 0;
+    const image_width: usize = @intCast(bmp_header.width);
+    const image_height: usize = if (flipped) @intCast(-bmp_header.height) else @intCast(bmp_header.height);
 
-    const alloc_size = bmp_header.width * 4 * bmp_header.height;
-    var pixels = try allocator.alloc(u32, @as(usize, @intCast(alloc_size)));
+    const pixel_byte_width = std.mem.alignForward(usize, bmp_header.bpp / 8, 4);
+    const row_bit_width: usize = @intCast(bmp_header.bpp * bmp_header.width);
+    const stride: usize = std.mem.alignForward(usize, row_bit_width / 32 * 4, 4);
+    var current_row = image_data.ptr + file_header.image_offset;
+
+    const alloc_size = image_width * 4 * image_height;
+    var pixels = try allocator.alloc(u32, alloc_size);
     var out_image = Image{
         .allocator = allocator,
         .pixels = pixels,
-        .width = @intCast(bmp_header.width),
-        .height = if (flipped) @intCast(-bmp_header.height) else @intCast(bmp_header.height),
+        .width = @intCast(image_width),
+        .height = @intCast(image_height),
         .bpp = bmp_header.bpp,
     };
 
-    var y: u32 = if (flipped) out_image.height - 1 else 0;
     if (flip_vertically) flipped = !flipped;
-    const increment = if (flipped) @as(i32, -1) else @as(i32, 1);
+    if (flipped) current_row += stride * (image_height - 1);
+    const stride_int: isize = @intCast(stride);
+    const increment: usize = if (flipped) @as(usize, @bitCast(-stride_int)) else @as(usize, @intCast(stride_int));
 
     switch (bmp_header.compression_type) {
         .rgb => {
-            for (0..out_image.height) |_| {
-                const scanline = y * out_image.width;
-                var x: u32 = 0;
-                for (0..out_image.width) |_| {
-                    const b = try reader.readByte();
-                    const g = try reader.readByte();
-                    const r = try reader.readByte();
+            for (0..image_height) |j| {
+                for (0..image_width) |i| {
+                    const b = current_row[i * pixel_byte_width];
+                    const g = current_row[i * pixel_byte_width + 1];
+                    const r = current_row[i * pixel_byte_width + 2];
                     const a = 0xFF;
                     const pixel: u32 = (@as(u32, a) << 24) | (@as(u32, b) << 16) | (@as(u32, g) << 8) | @as(u32, r);
-                    out_image.pixels[scanline + x] = pixel;
-                    x += 1;
+                    out_image.pixels[j * image_width + i] = pixel;
                 }
-                y += @bitCast(increment);
+                current_row += increment;
             }
         },
         .bitfields => {
@@ -142,21 +122,20 @@ pub fn load(allocator: mem.Allocator, filename: []const u8, flip_vertically: boo
             const b_shift: u5 = @truncate(@ctz(bmp_header.blue_mask));
             const a_shift: u5 = @truncate(@ctz(bmp_header.alpha_mask));
 
-            for (0..out_image.height) |_| {
-                const scanline = y * out_image.width;
-                var x: u32 = 0;
-                for (0..out_image.width) |_| {
-                    const value = try reader.readIntLittle(u32);
+            for (0..image_height) |j| {
+                for (0..image_width) |i| {
+                    const offset = i * pixel_byte_width;
+                    const ptr: *[4]u8 = @ptrCast(&current_row[offset]);
+                    const value = std.mem.bytesToValue(u32, ptr);
                     const r = (value & bmp_header.red_mask) >> r_shift;
                     const g = (value & bmp_header.green_mask) >> g_shift;
                     const b = (value & bmp_header.blue_mask) >> b_shift;
                     const a = (value & bmp_header.alpha_mask) >> a_shift;
 
                     const pixel = a << 24 | b << 16 | g << 8 | r;
-                    out_image.pixels[scanline + x] = pixel;
-                    x += 1;
+                    out_image.pixels[j * image_width + i] = pixel;
                 }
-                y += @bitCast(increment);
+                current_row += increment;
             }
         },
         else => return error.UnsupportedCompressionFormat,
