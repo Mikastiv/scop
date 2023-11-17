df1eaeed998e4777771056460606f2a5d7e7f635
Mikastiv <mikastiv@outlook.com>
2023-11-17 01:20:19 -0500

    matrix determinant

--- src/math.zig
+++ src/math.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub const Vec2 = [2]f32;
 pub const Vec3 = [3]f32;
 pub const Vec4 = [4]f32;
-pub const Mat2 = [2]f32;
+pub const Mat2 = [2]Vec2;
 pub const Mat3 = [3]Vec3;
 pub const Mat4 = [4]Vec4;
 
@@ -70,6 +70,15 @@ pub const vec = struct {
         };
     }
 
+    pub inline fn length2(v: anytype) f32 {
+        return switch (@TypeOf(v)) {
+            Vec2 => v[0] * v[0] + v[1] * v[1],
+            Vec3 => v[0] * v[0] + v[1] * v[1] + v[2] * v[2],
+            Vec4 => v[0] * v[0] + v[1] * v[1] + v[2] * v[2] + v[3] * v[3],
+            else => unsupportedType(@TypeOf(v)),
+        };
+    }
+
     pub inline fn unit(v: anytype) @TypeOf(v) {
         const len = length(v);
         return switch (@TypeOf(v)) {
@@ -98,6 +107,13 @@ pub const vec = struct {
             (a[1] * b[2]) - (a[2] * b[1]),
             (a[2] * b[0]) - (a[0] * b[2]),
             (a[0] * b[1]) - (a[1] * b[0]),
+        };
+    }
+
+    pub inline fn distance(a: anytype, b: @TypeOf(a)) f32 {
+        return switch (@TypeOf(a)) {
+            Vec2, Vec3, Vec4 => length(sub(a, b)),
+            else => unsupportedType(@TypeOf(a)),
         };
     }
 };
@@ -308,4 +324,80 @@ pub const mat = struct {
 
         return mat.mul(r, t);
     }
+
+    pub inline fn determinant(m: anytype) f32 {
+        return switch (@TypeOf(m)) {
+            Mat2 => m[0][0] * m[1][1] - m[1][0] * m[0][1],
+            Mat3 => {
+                const cofactor0 = determinant(Mat2{
+                    .{ m[1][1], m[2][1] },
+                    .{ m[1][2], m[2][2] },
+                });
+                const cofactor1 = determinant(Mat2{
+                    .{ m[0][1], m[2][1] },
+                    .{ m[0][2], m[2][2] },
+                });
+                const cofactor2 = determinant(Mat2{
+                    .{ m[0][1], m[1][1] },
+                    .{ m[0][2], m[1][2] },
+                });
+
+                return m[0][0] * cofactor0 - m[1][0] * cofactor1 + m[2][0] * cofactor2;
+            },
+            Mat4 => {
+                const cofactor0 = determinant(Mat3{
+                    .{ m[1][1], m[2][1], m[3][1] },
+                    .{ m[1][2], m[2][2], m[3][2] },
+                    .{ m[1][3], m[2][3], m[3][3] },
+                });
+                const cofactor1 = determinant(Mat3{
+                    .{ m[0][1], m[2][1], m[3][1] },
+                    .{ m[0][2], m[2][2], m[3][2] },
+                    .{ m[0][3], m[2][3], m[3][3] },
+                });
+                const cofactor2 = determinant(Mat3{
+                    .{ m[0][1], m[1][1], m[3][1] },
+                    .{ m[0][2], m[1][2], m[3][2] },
+                    .{ m[0][3], m[1][3], m[3][3] },
+                });
+                const cofactor3 = determinant(Mat3{
+                    .{ m[0][1], m[1][1], m[2][1] },
+                    .{ m[0][2], m[1][2], m[2][2] },
+                    .{ m[0][3], m[1][3], m[2][3] },
+                });
+
+                return m[0][0] * cofactor0 -
+                    m[1][0] * cofactor1 +
+                    m[2][0] * cofactor2 -
+                    m[3][0] * cofactor3;
+            },
+            else => unsupportedType(@TypeOf(m)),
+        };
+    }
 };
+
+// pub const Sphere = struct {
+//     const Self = @This();
+
+//     center: Vec3,
+//     radius: f32,
+
+//     pub fn contains(self: Self, point: Vec3) bool {
+//         return vec.distance(self.center, point) <= self.radius;
+//     }
+
+//     pub fn createCircumsphere(a: Vec3, b: Vec3, c: Vec3, d: Vec3) Self {
+//         const a2 = vec.length2(a);
+//         _ = a2;
+//         const b2 = vec.length2(b);
+//         _ = b2;
+//         const c2 = vec.length2(c);
+//         _ = c2;
+//         const d2 = vec.length2(d);
+//         _ = d2;
+//     }
+// };
+
+// pub fn smallestEnclosingSphere(points: []Vec3) Sphere {
+//     _ = points;
+// }
